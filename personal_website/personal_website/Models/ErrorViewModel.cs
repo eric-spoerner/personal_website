9a34db6056f5dde32afd9d47588f993b599ced0d
Eric Spoerner <espoerner@gmail.com>
2022-11-11 09:51:00 -0800

    Initial push.

---
+++ personal_website/personal_website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace personal_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

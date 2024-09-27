c77177ca9eaa8171af1fb3b475d0524fde20fd2c
Eric Spoerner <espoerner@gmail.com>
2024-09-27 12:39:54 -0700

    Added core schema and people table.

---
+++ schema/Tables/core.people.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS core.people;
+
+CREATE TABLE core.people(
+    ID SERIAL PRIMARY KEY -- creating new int-based PK
+	,ChadwickID varchar(10) NULL
+	,NameFirst varchar(25) NULL
+	,NameLast varchar(25) NULL
+    ,NameGiven varchar(100) NULL
+	,BirthYear smallint NULL
+	,BirthMonth tinyint NULL
+	,BirthDay tinyint NULL
+    -- --geographies may need to be normalized?  start with country and state
+	,BirthCountryID int NULL
+	,BirthStateID int NULL
+	-- birthState varchar(max) NULL,
+	-- birthCity varchar(max) NULL,
+	,DeathYear float NULL
+	,DeathMonth float NULL
+	,DeathDay float NULL
+    ,DeathCountryID int NULL
+	,DeathStateID int NULL
+	-- deathState varchar(max) NULL,
+	-- deathCity varchar(max) NULL,
+	,Weight float NULL
+	,Height float NULL
+    -- --bats/throws can be shrunk for sure
+	,Bats char NULL
+	,Throws char NULL
+    -- --why are debut and finalgame varchar?
+	,Debut date NULL
+	,FinalGame date NULL
+	,retroID varchar(10) NULL
+	,bbrefID varchar(10) NULL
+) ON PRIMARY;

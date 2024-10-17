8f3164224535a67377673e4ac0daf741307b388d
Eric Spoerner <espoerner@gmail.com>
2024-10-17 09:05:26 -0700

    EDA on ballparks, identifying duplicate names etc

--- EDA.sql
+++ EDA.sql
@@ -1,3 +1,4 @@
+/*
 select * from stg."chad_core_Parks";
 
 --IDs to import before teams:
@@ -87,31 +88,128 @@ with teams_parks as (
 SELECT "teamID", "franchID", "name", park, count(*) from stg."chad_core_Teams" group by "teamID", "franchID", "name", park
 )
 select * from teams_parks
+*/
 
-DROP TABLE IF EXISTS ballparks_stg;
+--start alias manipulation
+DROP TABLE IF EXISTS ballpark_stg;
 
 select *
-into temporary table ballparks_stg
+into temporary table ballpark_stg
 from stg."chad_core_Parks"; 
 --where "park.alias" like '%;%' or "park.alias" like '%/%';
 
-alter table ballparks_stg add column alias1 VARCHAR(100);
-alter table ballparks_stg add column alias2 VARCHAR(100);
-alter table ballparks_stg add column alias3 VARCHAR(100);
-alter table ballparks_stg add column alias4 VARCHAR(100);
-
-select * from ballparks_stg;
+alter table ballpark_stg add column alias_temp VARCHAR(100);
+alter table ballpark_stg add column alias1 VARCHAR(100);
+alter table ballpark_stg add column alias2 VARCHAR(100);
+alter table ballpark_stg add column alias3 VARCHAR(100);
+alter table ballpark_stg add column alias4 VARCHAR(100);
 
 --only slash is for "San Diego/Jack Murphy".  Don't worrry about that one procedurally, ahndle the semicolons
 
+update ballpark_stg set alias_temp = "park.alias"
+where "park.alias" is not null ;
+
+update ballpark_stg set alias1 = "park.alias", alias_temp=null where strpos("alias_temp", ';') = 0;
+
+update ballpark_stg
+set  	alias1 = trim(substring("park.alias", 0, strpos("park.alias", ';')))
+		,alias_temp =  trim(substring("alias_temp"
+						,strpos("alias_temp", ';') + 1
+						,length("alias_temp") - strpos("alias_temp", ';')))
+--from ballpark_stg
+where strpos("alias_temp", ';') > 0;
+
+update ballpark_stg set alias2 = "alias_temp", alias_temp=null where strpos("alias_temp", ';') = 0;
+
+update ballpark_stg
+set  	alias2 = trim(substring("alias_temp", 0, strpos("alias_temp", ';')))
+		,alias_temp =  trim(substring("alias_temp"
+						,strpos("alias_temp", ';') + 1
+						,length("alias_temp") - strpos("alias_temp", ';')))
+--from ballpark_stg
+	where strpos("alias_temp", ';') > 0;
+
+update ballpark_stg set alias3 = "alias_temp", alias_temp=null where strpos("alias_temp", ';') = 0;
+
+
+update ballpark_stg
+set  	alias3 = trim(substring("alias_temp", 0, strpos("alias_temp", ';')))
+		,alias_temp =  trim(substring("alias_temp"
+						,strpos("alias_temp", ';') + 1
+						,length("alias_temp") - strpos("alias_temp", ';')))
+--from ballpark_stg
+where "alias_temp" is not null 
+	and strpos("alias_temp", ';') > 0;
+
+update ballpark_stg set alias4 = "alias_temp", alias_temp=null where strpos("alias_temp", ';') = 0;
+
+alter table ballpark_stg drop column "park.alias";
+alter table ballpark_stg drop column "alias_temp";
+
+
+--select * from ballpark_stg;
+
+--end alias manipulation.  COMMENCE VERIFICATION.
+
+drop table if exists teams_parks;
+
+SELECT "teamID", "franchID", "name", park, count(*) 
+into temporary table teams_parks
+from stg."chad_core_Teams" 
+group by "teamID", "franchID", "name", park;
+
+select * from teams_parks;
+
+--most dupes on team parks are a result of multiple franchises playing in the same park, eg philly a's/phils
+;with dupes as (
+select "park", count(*) as cnt
+from teams_parks
+where "park" is not null
+group by "park"
+having count(*) > 1
+order by count(*) desc
+)
+select * 
+from dupes
+join teams_parks on teams_parks.park = dupes.park
+order by dupes.park;
+
+--which names are dupes that we should look out for?  check all names and aliases
+--9 total dupes across all names and aliases
+--couple are generic, wrigley field likely corresponds to early years of LA angels
+--consider appending city to name or something
+with allparknames as (
+	select "park.name" from ballpark_stg
+	union all
+	select "alias1" from ballpark_stg
+	union all
+	select "alias2" from ballpark_stg
+	union all
+	select "alias3" from ballpark_stg
+	union all
+	select "alias4" from ballpark_stg
+)
+select "park.name", count(*) 
+from ballpark_stg
+where "park.name" is not null
+group by "park.name"
+having count(*) > 1
+;
+
+
+
+/*
 select "park.name"
 		, "park.alias"
 		, strpos("park.alias", ';') 
-		, strpos("park.alias", '/') 
+		, trim(substring("park.alias", 0, strpos("park.alias", ';')))
+		, trim(substring("park.alias"
+					,strpos("park.alias", ';') + 1
+					,length("park.alias") - strpos("park.alias", ';')))
 from ballparks_stg
 where "park.alias" is not null 
-	and strpos("park.alias", ';') + strpos("park.alias", '/') > 0;
-
+	and strpos("park.alias", ';') > 0;
+*/
 
 
 --next: create the stadium table

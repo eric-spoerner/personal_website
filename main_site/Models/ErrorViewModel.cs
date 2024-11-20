97402742881dada6bfdc2e7b5b7e26febea85e56
Eric Spoerner <espoerner@gmail.com>
2024-11-20 15:24:45 -0800

    Purging .NET MVC stuff.  Too much unused bloat.

--- main_site/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace personal_website.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
